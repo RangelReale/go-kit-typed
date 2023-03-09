35e050b4de811872874cdccac6876c00d89c3207
Rangel Reale <rangelspam@gmail.com>
2023-03-09 15:59:50 -0300

    nats

--- transport/nats/publisher.go
+++ transport/nats/publisher.go
@@ -30,6 +30,44 @@ func NewPublisher[Req any, Resp any](
 	}
 }
 
+// NewPublisherStdEnc constructs a usable Publisher for a single remote method.
+func NewPublisherStdEnc[Req any, Resp any](
+	publisher *nats.Conn,
+	subject string,
+	enc gokitnatstransport.EncodeRequestFunc,
+	dec DecodeResponseFunc[Resp],
+	options ...gokitnatstransport.PublisherOption,
+) *Publisher[Req, Resp] {
+	pb := gokitnatstransport.NewPublisher(
+		publisher,
+		subject,
+		enc,
+		DecodeResponseFuncReverseAdapter(dec),
+		options...)
+	return &Publisher[Req, Resp]{
+		publisher: pb,
+	}
+}
+
+// NewPublisherStdDec constructs a usable Publisher for a single remote method.
+func NewPublisherStdDec[Req any, Resp any](
+	publisher *nats.Conn,
+	subject string,
+	enc EncodeRequestFunc[Req],
+	dec gokitnatstransport.DecodeResponseFunc,
+	options ...gokitnatstransport.PublisherOption,
+) *Publisher[Req, Resp] {
+	pb := gokitnatstransport.NewPublisher(
+		publisher,
+		subject,
+		EncodeRequestFuncReverseAdapter(enc),
+		dec,
+		options...)
+	return &Publisher[Req, Resp]{
+		publisher: pb,
+	}
+}
+
 // Endpoint returns a usable endpoint that invokes the remote endpoint.
 func (p Publisher[Req, Resp]) Endpoint() endpoint.Endpoint[Req, Resp] {
 	return endpoint.Adapter[Req, Resp](p.publisher.Endpoint())
