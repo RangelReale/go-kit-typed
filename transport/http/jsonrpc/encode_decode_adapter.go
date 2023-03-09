cad7a49b53bccdecc9cabc0147993dd4f0a9eb9d
Rangel Reale <rangelspam@gmail.com>
2023-03-09 20:57:26 -0300

    separate reverse adapter

--- transport/http/jsonrpc/encode_decode_adapter.go
+++ transport/http/jsonrpc/encode_decode_adapter.go
@@ -29,15 +29,6 @@ func EndpointCodecAdapter[Req any, Resp any](codec gokitjsonrpctransport.Endpoin
 	}
 }
 
-// EndpointCodecReverseAdapter is an adapter to the non-generic EndpointCodec type
-func EndpointCodecReverseAdapter[Req any, Resp any](codec EndpointCodec[Req, Resp]) gokitjsonrpctransport.EndpointCodec {
-	return gokitjsonrpctransport.EndpointCodec{
-		Endpoint: endpoint.ReverseAdapter[Req, Resp](codec.Endpoint),
-		Decode:   DecodeRequestFuncReverseAdapter(codec.Decode),
-		Encode:   EncodeResponseFuncReverseAdapter(codec.Encode),
-	}
-}
-
 // DecodeRequestFuncAdapter is an adapter from the non-generic DecodeRequestFunc function
 func DecodeRequestFuncAdapter[Req any](f gokitjsonrpctransport.DecodeRequestFunc) DecodeRequestFunc[Req] {
 	return func(ctx context.Context, message json.RawMessage) (Req, error) {
@@ -45,41 +36,9 @@ func DecodeRequestFuncAdapter[Req any](f gokitjsonrpctransport.DecodeRequestFunc
 	}
 }
 
-// DecodeRequestFuncReverseAdapter is an adapter to the non-generic DecodeRequestFunc function
-func DecodeRequestFuncReverseAdapter[Req any](f DecodeRequestFunc[Req]) gokitjsonrpctransport.DecodeRequestFunc {
-	return func(ctx context.Context, message json.RawMessage) (interface{}, error) {
-		return f(ctx, message)
-	}
-}
-
 // EncodeResponseFuncAdapter is an adapter from the non-generic EncodeResponseFunc function
 func EncodeResponseFuncAdapter[Resp any](f gokitjsonrpctransport.EncodeResponseFunc) EncodeResponseFunc[Resp] {
 	return func(ctx context.Context, resp Resp) (json.RawMessage, error) {
 		return f(ctx, resp)
-	}
-}
-
-// EncodeResponseFuncReverseAdapter is an adapter to the non-generic EncodeResponseFunc function
-func EncodeResponseFuncReverseAdapter[Resp any](f EncodeResponseFunc[Resp]) gokitjsonrpctransport.EncodeResponseFunc {
-	return func(ctx context.Context, i interface{}) (json.RawMessage, error) {
-		return util.CallTypeResponseWithError[Resp, json.RawMessage](i, func(r Resp) (json.RawMessage, error) {
-			return f(ctx, r)
-		})
-	}
-}
-
-// EncodeRequestFuncReverseAdapter is an adapter to the non-generic EncodeRequestFunc function
-func EncodeRequestFuncReverseAdapter[Req any](f EncodeRequestFunc[Req]) gokitjsonrpctransport.EncodeRequestFunc {
-	return func(ctx context.Context, i interface{}) (json.RawMessage, error) {
-		return util.CallTypeResponseWithError[Req, json.RawMessage](i, func(r Req) (json.RawMessage, error) {
-			return f(ctx, r)
-		})
-	}
-}
-
-// DecodeResponseFuncReverseAdapter is an adapter to the non-generic DecodeResponseFunc function
-func DecodeResponseFuncReverseAdapter[Resp any](f DecodeResponseFunc[Resp]) gokitjsonrpctransport.DecodeResponseFunc {
-	return func(ctx context.Context, response gokitjsonrpctransport.Response) (interface{}, error) {
-		return f(ctx, response)
 	}
 }
